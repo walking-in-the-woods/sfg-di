bcd5281e764ed9607de0b05e5aa5e6bc357cf1e8
Alex S <as202009261140@gmail.com>
2021-02-20 11:48:26 +0300

    Property Source

--- src/main/java/as/sfgdi/SfgDiApplication.java
+++ src/main/java/as/sfgdi/SfgDiApplication.java
@@ -1,9 +1,7 @@
 package as.sfgdi;
 
-import as.sfgdi.controllers.ConstructorController;
 import as.sfgdi.controllers.MyController;
-import as.sfgdi.controllers.PropertyController;
-import as.sfgdi.controllers.SetterController;
+import as.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,11 +13,9 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.println(myController.hello());
 
-		System.out.println(ctx.getBean(PropertyController.class).sayHello());
-		System.out.println(ctx.getBean(SetterController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
 	}
 
 }
---
+++ src/main/java/as/sfgdi/config/PropertyConfig.java
@@ -0,0 +1,38 @@
+package as.sfgdi.config;
+
+import as.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${as.username}")
+    String user;
+
+    @Value("${as.password}")
+    String password;
+
+    @Value("${as.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+                new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
